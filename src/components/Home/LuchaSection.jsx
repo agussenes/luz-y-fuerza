7a26902393b764d7f619154f5f68b9fb0db44356
agussenes <agustinsenestrari99@gmail.com>
2025-08-21 14:59:56 -0300

    propuesta para el segundo home

--- src/components/Home/LuchaSection.jsx
+++ src/components/Home/LuchaSection.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import CountUp from 'react-countup';
-import {Container, Row, Col} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 // imagenes 
-import fondoLucha from '../../assets/images/home/bg-counter.jpg';
+import fondoLucha from '../../assets/images/home/bgCounter2.webp';
 import imgCounter from '../../assets/images/home/img-counter.jpg';
 
 function LuchaSection() {
@@ -13,7 +13,10 @@ function LuchaSection() {
             <section
                 className="modulo-lucha  text-white py-5"
                 style={{
-                    backgroundImage: `url(${fondoLucha})`,
+                    backgroundImage: `linear-gradient(
+                    rgba(0,0,0,0.5),     /* filtro oscuro arriba */
+                    rgba(0,0,0,0.5)      /* filtro oscuro abajo */
+                    ), url(${fondoLucha})`,
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                     backgroundRepeat: 'no-repeat',
@@ -22,9 +25,9 @@ function LuchaSection() {
                 <div className="py-5">
                     <Container>
                         <Row className="align-items-center">
-                            <Col md={6}>
+                            <Col md={8}>
                                 <p className="text-info fw-bold mb-2">LUZ Y FUERZA CÓRDOBA</p>
-                                <h2 className="mb-4">Defendiendo un Sindicato participativo y plural en permanente transformación.</h2>
+                                <h2 className="mb-4 display-4">Defendiendo un Sindicato participativo y plural en permanente transformación.</h2>
 
                                 <Link to="/nuestra-lucha" className="counter-box text-decoration-none">
                                     <div className="d-inline-flex align-items-center gap-3 px-4 py-3 rounded shadow-sm bg-white bg-opacity-10 border border-info backdrop-blur">
@@ -35,7 +38,7 @@ function LuchaSection() {
                                     </div>
                                 </Link>
                             </Col>
-                            <Col md={6} className='d-flex justify-content-center'>
+                            <Col md={4} className='d-flex justify-content-center'>
                                 <img src={imgCounter} alt="Historia de lucha" className=" my-4 rounded shadow" loading='lazy' height={400} />
                             </Col>
                         </Row>
